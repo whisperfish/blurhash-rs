d3b013245410d3c86024391d53211c8b1ab90888
Ardy Febriansyah <Ardyfeb02@gmail.com>
2021-02-17 23:11:22 +0700

    fix(encoding): make ac values absolute
    https://github.com/Raincal/blurhash-rs/issues/1

--- src/lib.rs
+++ src/lib.rs
@@ -65,9 +65,9 @@ pub fn encode(
     if !ac.is_empty() {
         let mut actualmaximum_value = 0.0;
         for i in 0..components_y * components_x - 1 {
-            actualmaximum_value = f32::max(ac[i as usize][0], actualmaximum_value);
-            actualmaximum_value = f32::max(ac[i as usize][1], actualmaximum_value);
-            actualmaximum_value = f32::max(ac[i as usize][2], actualmaximum_value);
+            actualmaximum_value = f32::max(f32::abs(ac[i as usize][0]), actualmaximum_value);
+            actualmaximum_value = f32::max(f32::abs(ac[i as usize][1]), actualmaximum_value);
+            actualmaximum_value = f32::max(f32::abs(ac[i as usize][2]), actualmaximum_value);
         }
 
         let quantised_maximum_value = f32::max(
